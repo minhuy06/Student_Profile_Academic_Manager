f89076224068064fbcc589b50078234b3c5735d2
Võ Minh Huy <1410huyvo@gmail.com>
2026-09-25 20:39:49 +0700

    Add Student model and ViewExtensions utility functions

---
+++ app/src/main/java/com/ute/student_profile_academic_manager/model/Student.kt
@@ -0,0 +1,14 @@
+package com.ute.student_profile_academic_manager.model
+
+import java.io.Serializable
+
+data class Student(
+    val id: String,
+    val name: String,
+    val className: String,
+    val email: String,
+    val gpa: Double
+) : Serializable {
+    val isHonorStudent: Boolean
+        get() = gpa >= 3.6
+}
